30bf5a8808f520da0e46d8c59dbeed920043664d
Mindaugas Jačionis <m.jacionis@gmail.com>
2021-04-13 21:02:26 +0300

    Add current group names to assignment

--- routes/weekThree/utils/generatePartyGuests.js
+++ routes/weekThree/utils/generatePartyGuests.js
@@ -2,21 +2,27 @@ const { name, random } = require("faker");
 const { firstName, lastName } = name;
 const { uuid, boolean } = random;
 const myStudents = [
-  "Erikas",
-  "Milda",
-  "Živilė",
-  "Benjamin",
-  "Džiugas",
+  "Agnė",
+  "Aivaras",
+  "Aurimas",
+  "Benjaminas",
   "Gediminas",
-  "Grantas",
   "Iveta",
-  "Karolis",
-  "Lukas",
-  "Radvilė",
-  "Robertas"
+  "Jonas",
+  "Laura",
+  "Mantas",
+  "Martynas",
+  "Mindaugas",
+  "Osvaldas",
+  "Povilas",
+  "Silvija",
+  "Tautvydas",
+  "Vilmantas",
+  "Vladislav",
+  "Žilvinas",
 ];
 
-const generatePartyGuests = key => {
+const generatePartyGuests = (key) => {
   const arrayLength = myStudents.length * 10 + 9;
   const result = [...Array(arrayLength)].map((v, i) => {
     const insertStudent = i % 10 === 0 && i !== 0;
@@ -26,7 +32,7 @@ const generatePartyGuests = key => {
       name: insertStudent ? myStudents[i / 10 - 1] : fullName,
       id: uuid(),
       [key]: insertStudent || boolean(),
-      attending: insertStudent || boolean()
+      attending: insertStudent || boolean(),
     };
   });
 
